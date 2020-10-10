e20896d3d01d6daebe1b4ee54758b57371de306d
nonicalx <nonicalx@gmail.com>
2020-10-10 20:03:50 +0100

    completed the basic configuration of webpack for react

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,36 @@
 const path = require('path');
 
-const HtmlWebpack = require('html-webpack-plugin');
+
+ const HtmlWebpack = require('html-webpack-plugin');
 
 
 
 
 module.exports = {
-    entry: "./src/index.js"
+    entry: "./src/index.js",
+    output: {
+        path: path.join(__dirname, './dist'),
+        filename: "bundled.js"
+    },
+
+
+    module:{
+        rules:[
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use:{
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
+
+
+
+    plugins:[
+        new HtmlWebpack({
+            template: './public/index.html',
+        })
+    ]
 }
