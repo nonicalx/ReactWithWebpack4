d829a6afcdacdc8fa05772398fc14910d35115b4
nonicalx <nonicalx@gmail.com>
2020-10-10 20:07:07 +0100

    created App.js to test webpack configuration

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+const App =()=>{
+
+
+    return(
+        <div>
+            <h1>4 pack React</h1>
+            <p>off to a good start Amen!!!</p>
+        </div>
+    )
+}
+
+export default App;
