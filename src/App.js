b5a7fb527c7c550a9f3fb6f7a055af840f506a68
oranye nonso (blue blooded) <n.oranye@genesystechhub.com>
2020-10-11 13:10:27 +0100

    modified App.js, created app.css and added images to test out webpack's new configs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react'
-
+import Royale from './images/royale.PNG'
+import Victory from './images/victory.gif'
+import './app.css'
 
 
 const App =()=>{
 
 
     return(
-        <div>
+        <div className="app">
             <h1>4 pack React</h1>
-            <p>off to a good start Amen!!!</p>
+            <p>off to a good start Amen!</p>
+            <div>
+            <img src={Victory}/>
+            <img src={Royale}/>
+            </div>
         </div>
     )
 }
